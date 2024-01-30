31be3c9f39a11e793155a5a1ba532e4f0368c6e8
avi.deshmukh1998@gmail.com <avi.deshmukh1998@gmail.com>
2024-01-31 00:06:29 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermConditionComponent } from './pages/term-condition/term-condition.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'category',
+    component: SingleCategoryComponent,
+  },
+  {
+    path:'post',
+    component:SinglePostComponent
+  },
+  {
+    path:'about',
+    component:AboutUsComponent
+  },
+  {
+    path:'term-condition',
+    component:TermConditionComponent
+  },
+  {
+    path:'contact',
+    component:ContactUsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
